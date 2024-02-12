967c25e789745321cd2af5ab3c8080a411278283
Haris Alic <haris.alic@students.fhnw.ch>
2024-02-12 12:12:19 +0000

    Feature: Calculate the average of some numbers

--- foo.py
+++ foo.py
@@ -1,5 +1,21 @@
-def another_string():
-	print("Another string")
+import argparse
+
+
+def average(numbers: list) -> float:
+    return sum(numbers) / len(numbers)
+
 
 if __name__ == "__main__":
-	another_string()
+    parser = argparse.ArgumentParser(
+        description="Calculate the average of some numbers"
+    )
+    parser.add_argument(
+        "numbers",
+        metavar="N",
+        type=float,
+        nargs="+",
+        help="The numbers separated by whitespace",
+    )
+    args = parser.parse_args()
+    result = average(args.numbers)
+    print(result)
