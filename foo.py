f9ef814ea16b0d62dae5c73fbcc6cf002db3d920
simonebecher <159783640+simonebecher@users.noreply.github.com>
2024-02-12 12:51:34 +0000

    Refactor: Add min and max functionality

--- foo.py
+++ foo.py
@@ -1,13 +1,13 @@
 import argparse
 
 
-def average(numbers: list) -> float:
-    return sum(numbers) / len(numbers)
+def average_max_min(numbers: list) -> float:
+    return sum(numbers) / len(numbers), max(numbers), min(numbers)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Calculate the average of some numbers"
+        description="Returns average, max and min of given numbers"
     )
     parser.add_argument(
         "numbers",
@@ -17,5 +17,5 @@ if __name__ == "__main__":
         help="The numbers separated by whitespace",
     )
     args = parser.parse_args()
-    result = average(args.numbers)
+    result = average_max_min(args.numbers)
     print(result)
